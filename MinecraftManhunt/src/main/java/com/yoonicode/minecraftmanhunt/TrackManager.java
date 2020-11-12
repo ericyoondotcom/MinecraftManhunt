41bab07490b563ec93d0ab0c799b5610ffa529fe
Eric <emjyoon@gmail.com>
2020-11-11 20:28:04 -0800

    Add new music tracks

--- MinecraftManhunt/src/main/java/com/yoonicode/minecraftmanhunt/TrackManager.java
+++ MinecraftManhunt/src/main/java/com/yoonicode/minecraftmanhunt/TrackManager.java
@@ -44,6 +44,7 @@ public class TrackManager extends AudioEventAdapter implements Listener {
 
     public HashMap<String, String> trackURLs = new HashMap<String, String>() {{
         put("intro", "https://www.youtube.com/watch?v=beCC9xJjLZQ"); // Epic Dawn - Bobby Cole
+        put("intro2", "https://www.youtube.com/watch?v=JoTEaIMMnbE"); // Forgotten Ones - Will Van De Crommert via. Cinematic Sky
         put("headstart", "https://www.youtube.com/watch?v=tJFqdLg58i4"); // Trance Music for Racing Game - Bobby Cole
         put("pirates", "https://www.youtube.com/watch?v=27mB8verLK8"); // Pirates of the Caribbean theme song
         put("gatheringresources", "https://www.youtube.com/watch?v=tKfpwSVpXxU"); // Do the Funky Strut - Bobby Cole
@@ -53,25 +54,38 @@ public class TrackManager extends AudioEventAdapter implements Listener {
         put("relaxing", "https://www.youtube.com/watch?v=ipCeVg-SA-Y"); // Inhale and Exhale - For Peace of Mind and Serenity
         put("fun", "https://www.youtube.com/watch?v=O9cWfV_3J7o"); // Playtime - Peter Godfrey
         put("chill", "https://www.youtube.com/watch?v=eSNXvU-kowc"); // Crime and Robbery Music
+        put("upbeat", "https://www.youtube.com/watch?v=rxfqhAOfIqo"); // Ready and Go - Diego Martinez
+        put("swing", "https://www.youtube.com/watch?v=wJ5Ip9qs9iw"); // Snap Swing - Diego Martinez
+        put("jazz", "https://www.youtube.com/watch?v=R_-44VsmTmI"); // Heist Prop Montage - Rob McAllister
+        put("upbeat-bite", "https://www.youtube.com/watch?v=hn7k7z_heOw");
 
         put("preparing-safe", "https://www.youtube.com/watch?v=gehk17hjEdU"); // Race Against Time - Ceiri Torjussen
         put("preparing-danger", "https://www.youtube.com/watch?v=CYcTNDXUOoY"); // Action Preparation - Jason Donnelly
         put("lowdanger", "https://www.youtube.com/watch?v=3OSs540jvF4"); // White Lines - Zac Nelson
+        put("journey", "https://www.youtube.com/watch?v=7we8rs_YF5w"); // Crucial Conflict - Westar Music
+        put("cinematic", "https://www.youtube.com/watch?v=K1p5TCW2tp0"); // Hidden in the Corner - Marc Robillard
+        put("premonition", "https://www.youtube.com/watch?v=pNxcUi50Ge0"); // Evolution of Man - Linus Lau
 
         put("risingaction", "https://www.youtube.com/watch?v=ghAeHHTID2k"); // Strange Things - Zac Nelson
+        put("rhythmic", "https://www.youtube.com/watch?v=vWghhruheNk"); // Escape Theme - Udeze Ukwuoma via. Von Neumann Effect
 
         put("nether", "https://www.youtube.com/watch?v=tQdAiG29HiM"); // Music For a Killer - Bobby Cole
 
-        put("spooked", "https://www.youtube.com/watch?v=3kclVzQ3S4M"); // Re-Animation - Peter Godfrey
+        put("spooked", "https://www.youtube.com/watch?v=3kclVzQ3S4M"); // Re-Animation - Peter Godfrey / When Bats Fly - Neil Cross
+        put("spooked2", "https://www.youtube.com/watch?v=UZ7OcaFTiA4"); // The Final Decision - Clark Aboud
         put("tense", "https://www.youtube.com/watch?v=Vv9-cxbacOg"); // Tension in the Air - Bobby Cole
         put("plotting", "https://www.youtube.com/watch?v=RXyYt8kx740"); // Thinking and Tension - Bobby Cole
         put("approaching", "https://www.youtube.com/watch?v=wD3Mf4asnFk"); // Thinking About Murder - Boby Cole
+        put("dramatic", "https://www.youtube.com/watch?v=-u7MW_KkTWU"); // Dramatic Movie Opening - Bobby Cole
 
         put("danger", "https://www.youtube.com/watch?v=kVJQ74RoHCM"); // Dramatic Anticipation (Alternative version) - PremiumTrax
         put("danger2", "https://www.youtube.com/watch?v=5y4b7jDXf_E"); // Dark Time Ticking - Robert Valenti
 
         put("discovered", "https://www.youtube.com/watch?v=BMUPNgU3lII"); // Evidence - RTaphael Costa
         put("found", "https://www.youtube.com/watch?v=YMwQQJ0ChCU"); // On The Killer's Trail - William Pearson & Robert Watson
+
+        put("intense", "https://www.youtube.com/watch?v=VNTLefY9rT0"); // Promo Upbeat Intense Racer 1 - Jermaine Stegall
+        put("intense2", "https://www.youtube.com/watch?v=KwxIN6-BNWs"); // Fight to the Death - Bobby Cole
 
         // on runner hit, less than two pieces of armor
         put("chase", "https://www.youtube.com/watch?v=CX9wFdExF_k"); // Navajo Race - Bryan Steele
@@ -81,6 +95,7 @@ public class TrackManager extends AudioEventAdapter implements Listener {
         put("epicwar", "https://www.youtube.com/watch?v=_UBZmrQwD9o"); // There Is No Escape - Hollywood Film Music
         put("endwar", "https://www.youtube.com/watch?v=0EsBItv1Pns"); // Heist Gone Wrong - Clark Aboud
         put("fighting", "https://www.youtube.com/watch?v=S4MC7QdayXc"); // Halo - Michael Vignola
+        put("heroic", "https://www.youtube.com/watch?v=eGojBSVYHZA"); // Heroic Fireworks - Paul Werner
 
         // On hunter kill by runner
         put("beastmode", "https://www.youtube.com/watch?v=4rGl1KXV4eA"); // Dominating - Ray Aley
@@ -92,7 +107,8 @@ public class TrackManager extends AudioEventAdapter implements Listener {
         put("loss", "https://www.youtube.com/watch?v=9Fx314TyuWI"); // Mystery Collection - Lonesome Piano - D. Silverstone
         put("sad", "https://www.youtube.com/watch?v=OK7a4EQVRc0"); // Sad Goodbyes - Mark Kueffner
 
-        put("timesup", "https://soundcloud.com/user-547037461/clock-is-ticking-benny-hawes"); // Clock Is Ticking - Benny Hawes
+        put("timesup", "https://www.youtube.com/watch?v=dkqslkBUY6E"); // Clock Is Ticking - Benny Hawes
+        put("finale", "https://www.youtube.com/watch?v=rQUXUdD3B-4"); // Reach Beyond - Paul Werner
     }};
     public HashMap<String, AudioTrack> tracks = new HashMap<String, AudioTrack>();
 
@@ -226,32 +242,44 @@ public class TrackManager extends AudioEventAdapter implements Listener {
         if(dangerLevel == null) return;
         switch(dangerLevel){
             case Chasing:
-                candidates.add("danger");
+                candidates.add("danger2");
                 candidates.add("risingaction");
+                candidates.add("endwar");
                 break;
             case InSight:
+                candidates.add("danger");
                 candidates.add("preparing-danger");
-                candidates.add("danger2");
                 candidates.add("timesup");
+                break;
             case Stealth:
                 candidates.add("approaching");
                 candidates.add("found");
                 candidates.add("plotting");
+                candidates.add("discovered");
+                break;
             case Approaching:
                 candidates.add("relaxing");
-                candidates.add("discovered");
                 candidates.add("tense");
                 candidates.add("resolution");
                 candidates.add("resolution2");
+                candidates.add("rhythmic");
+                candidates.add("premonition");
+                break;
             case FarAway:
                 candidates.add("fun");
                 candidates.add("preparing-safe");
                 candidates.add("chill");
                 candidates.add("gatheringresources");
                 candidates.add("lowdanger");
+                candidates.add("journey");
+                candidates.add("swing");
+                break;
             case RunnerInNether:
                 candidates.add("nether");
                 candidates.add("spooked");
+                candidates.add("cinematic");
+                candidates.add("jazz");
+                break;
         }
         int random = (int)(Math.random() * candidates.size());
         playTrack(candidates.get(random));
@@ -393,14 +421,17 @@ public class TrackManager extends AudioEventAdapter implements Listener {
         }else{
             return;
         }
-        int random = (int)(Math.random() * 3);
+
         long armorCount = Arrays.stream(damager.getInventory().getArmorContents()).filter(stack -> stack != null).count() +
                 Arrays.stream(damagee.getInventory().getArmorContents()).filter(stack -> stack != null).count();
         if(armorCount > 2){
+            int random = (int)(Math.random() * 4);
             if(random == 0) playSpecialTrack("epicwar");
-            else if(random == 1) playSpecialTrack("endwar");
+            else if(random == 1) playSpecialTrack("dramatic");
+            else if(random == 2) playSpecialTrack("heroic");
             else playSpecialTrack("fighting");
         }else{
+            int random = (int)(Math.random() * 2);
             if(random == 0) playSpecialTrack("chase");
             else playSpecialTrack("chase2");
         }
@@ -409,7 +440,9 @@ public class TrackManager extends AudioEventAdapter implements Listener {
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event){
         if(main.hunters.contains(event.getEntity().getName())){
-            playSpecialTrack("beastmode", true);
+            int random = (int)(Math.random() * 2);
+            if(random == 0) playSpecialTrack("beastmode", true);
+            else playSpecialTrack("upbeat", true);
         }else if(main.runners.contains(event.getEntity().getName())){
             for(String i : main.runners){
                 Player p = Bukkit.getPlayer(i);
